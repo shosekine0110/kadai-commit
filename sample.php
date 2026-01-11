eb9083040c5127b890731eb851717137cf8a6bbb
shou.sekine <shosekine0110@gmail.com>
2026-01-11 17:21:49 +0900

    Add sample.php for omikuji program

--- sample.php
+++ sample.php
@@ -1,15 +1,4 @@
-<?php
-    $result = '無し';
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && array_key_exists('act', $_POST)) {
-        $result = omikuji();
-    }
-
-    function omikuji() {
-        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
-        return $fortune[random_int(0, count($fortune) - 1)];
-    }
-?>
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
@@ -17,9 +6,10 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <p>おみくじの結果：無し</p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
 </html>
+
